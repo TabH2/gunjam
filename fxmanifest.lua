3d23c763f74ca52a69165a940cd408deaef65d14
TabH2 <149743341+TabH2@users.noreply.github.com>
2023-12-28 16:14:15 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Ziggy & Tab'
+description 'Gun Jam Script'
+version '1.0.0'
+
+client_scripts {
+    'cl_gunjam.lua',
+}
+
+files {
+    'html/jamed.ogg',
+    'html/unjam.ogg',
+}
